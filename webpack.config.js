ab35be50e2ccde1912436b37f5b2917d3202a42b
tomotetra <cure.tomoki@gmail.com>
2020-06-25 22:11:58 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+const target = process.env.NODE_ENV || 'development';
+const isDev = target === 'development';
+
+const resolve = (dir) => path.resolve(__dirname, dir);
+
+module.exports = {
+  mode: target,
+  target: 'web',
+  entry: ['./src/index.tsx'],
+  output: {
+    path: resolve('dist'),
+    publicPath: '/',
+    filename: '[name].[hash].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': resolve('src'),
+    },
+  },
+  devtool: isDev && 'inline-source-map',
+  devServer: {
+    contentBase: resolve('dist'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(target),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
